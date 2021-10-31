b8c94160b2225a6e6700ddb1c473e839278b339f
Liz Piedra <lizpiedra@lizs-mbp-2.lan>
2021-10-30 20:49:09 -0400

    Add repository to get data from db. Add temporary view to test that the data can be retrieve.

---
+++ PlanMyTrip/PlanMyTrip/Data/TripRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PlanMyTrip.Data.Entities;
+
+namespace PlanMyTrip.Data
+{
+    public class TripRepository : ITripRepository
+    {
+        private readonly TripContext _context;
+
+        public TripRepository(TripContext context)
+        {
+            this._context = context;
+        }
+
+        /// <summary>
+        ///  This method gets all users stored on the database
+        /// </summary>
+        /// <returns>All users records stored on the database</returns>
+        public IEnumerable<User> GetAllUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        /// <summary>
+        /// This method gets the user record with the given username
+        /// </summary>
+        /// <param name="userName">The userName of the user to be returned</param>
+        /// <returns>User record with the given userName</returns>
+        public IEnumerable<User> GetUserByUsername(string userName)
+        {
+            return _context.Users.Where(c => c.UserName == userName).ToList();
+        }
+
+        /// <summary>
+        /// This method saves all changes made to the db records
+        /// </summary>
+        /// <returns>True if at least one row was changed</returns>
+        public bool saveAllChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
--- PlanMyTrip/PlanMyTrip/Startup.cs
+++ PlanMyTrip/PlanMyTrip/Startup.cs
@@ -25,6 +25,7 @@ namespace PlanMyTrip
         {
             services.AddControllersWithViews();
             services.AddDbContext<TripContext>();
+            services.AddScoped<ITripRepository, TripRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
